f48aacf887a851c064ad3a275d29ae647696cb52
gnikonov <gnikonov@mts.ru>
2020-06-27 21:19:12 +0300

    add static code analyse

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -1,6 +1,10 @@
 package com.leszko.calculator;
 import org.springframework.stereotype.Service;
 
+
+/**
+ * Main Spring Application.
+ */
 @Service
 public class Calculator {
      int sum(int a, int b) {
